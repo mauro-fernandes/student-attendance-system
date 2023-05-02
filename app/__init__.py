efb47f4b562497cabd84764ca0ecc2497f34de0f
Mauro F Almeida <maurofda@gmail.com>
2023-05-02 12:19:39 -0300

    bootstrap layout

--- app/__init__.py
+++ app/__init__.py
@@ -9,45 +9,17 @@ app = Flask(__name__)
 
 app.config.from_object('config')
 
-## sqlite://<nohostname>/<path>
-## where <path> is relative:
-engine = create_engine("sqlite:///storage.sqlite3")
-
 db = SQLAlchemy(app)
 
-#db.app = app
-#db.init_app(app)
-#db.create_all()
-#
-
-
-'''def setup_db(app, database_path='sqlite:///DB.sqlite3'):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-    db.app = app
-    db.init_app(app)
-    db.create_all()'''
-
-
 migrate = Migrate(app, db)
-
-#app.add_command('db',  )
-
-
 
 from app.controllers import defaut
 
 
-
-
 def test_connection(self):
     with app.app_context():
+        db.app = app
+        db.init_app(app)
         db.create_all()
         app.runserver()
-        #test code
         
-
-
-if __name__ == "__main__":
-    db.create_all()
-    app.runserver()
